68ff3214a8a65c659e388972108f797c5f914000
KingCroods <kevinfernando7561@gmail.com>
2025-11-19 08:59:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?1zlcSB'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?dLdz3s'
 };
